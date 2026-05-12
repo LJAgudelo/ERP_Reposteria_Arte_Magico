4256c27c1873f7bb67d7020661a3e53e5c903a31
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2026-05-11 21:10:49 -0500

    Se crea controller de proveedores

--- backend/src/modules/proveedores/proveedor.controller.js
+++ backend/src/modules/proveedores/proveedor.controller.js
@@ -1,1 +1,17 @@
-//Define logica
+const proveedoresModel = require("./proveedor.model");
+
+const crearProveedor= async(req, res, next)=>{
+try{
+    const proveedor = await proveedoresModel.crearProveedor(req.body);
+    res.status(201).json({
+        ok: true,
+        data: proveedor
+    });
+}catch (error){
+    next(error)
+}
+}
+module.exports={
+    crearProveedor
+
+}
