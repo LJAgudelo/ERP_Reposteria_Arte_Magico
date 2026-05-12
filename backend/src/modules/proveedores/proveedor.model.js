67df1697b9c111fd072bffb6b8d1fc9610c7da45
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2026-05-11 20:58:54 -0500

    Se crea model de proveedores

--- backend/src/modules/proveedores/proveedor.model.js
+++ backend/src/modules/proveedores/proveedor.model.js
@@ -1,1 +1,26 @@
-// Define  de datos
+const pool = require('../../config/db');
+
+const crearProveedor = async (proveedor) => {
+
+    const query= `INSERT INTO proveedores (nombre, numero_nit, direccion, telefono, correo, estado ) 
+    VALUES ($1, $2, $3, $4, $5, $6 ) 
+    RETURNING *
+    `;
+    
+    const values = [
+        proveedor.nombre,
+        proveedor.numero_nit,
+        proveedor.direccion,
+        proveedor.telefono,
+        proveedor.correo,
+        proveedor.estado
+    ];
+
+    const result = await pool.query(query, values);
+
+    return result.rows[0];
+}
+
+module.exports={
+    crearProveedor
+}
